c9c49ae64ce4d79d108fe3f0bf5baade0e437bc1
BhaktiKhichade <bhaktikhichade@gmail.com>
2023-04-03 19:25:55 +0530

    A new feature to update vehicle data

--- Managing_Vehicle_data/src/main/java/org/in/vehicle/controller/VehicleDataController.java
+++ Managing_Vehicle_data/src/main/java/org/in/vehicle/controller/VehicleDataController.java
@@ -2,11 +2,13 @@ package org.in.vehicle.controller;
 
 import org.in.vehicle.model.CarAddRequest;
 import org.in.vehicle.service.VehicleService;
+import org.in.vehicle.service.exceptions.InvalidRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +37,23 @@ public class VehicleDataController {
             return ResponseEntity.status(HttpStatus.OK).body(vehicleService.getCarDetailsById(vehicleId));
         }catch (InvalidRequestException e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getLocalizedMessage());
+        }
+    }
+    
+    @PutMapping("/cars/{vehicleId}")
+    public ResponseEntity<?> updateCarDetails(@PathVariable("vehicleId")Integer vehicleId,
+                                              @RequestBody CarAddRequest request){
+        try {
+            CarAddRequest carAddRequest = vehicleService.getCarDetailsById(vehicleId);
+            request.setVehicleId(vehicleId);
+            request.getDetails().setDetailsId(carAddRequest.getDetails().getDetailsId());
+            request.getLocation().setLocationId(carAddRequest.getLocation().getLocationId());
+            request.getDetails().getManufacturer().setManufacturerId(carAddRequest.getDetails().getManufacturer().getManufacturerId());
+            return ResponseEntity.status(HttpStatus.OK).body(vehicleService.saveCarDetails(request));
+        }catch (InvalidRequestException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getLocalizedMessage());
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getLocalizedMessage());
         }
     }
 }
