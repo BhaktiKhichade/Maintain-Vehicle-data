13d54e17652a4046b0bbfa7094996d7de6162b47
BhaktiKhichade <bhaktikhichade@gmail.com>
2023-04-03 22:10:17 +0530

    A unit test cases for fetch car details by id functionality

--- Managing_Vehicle_data/src/test/java/org/in/vehicle/mock/VehicleServiceMock.java
+++ Managing_Vehicle_data/src/test/java/org/in/vehicle/mock/VehicleServiceMock.java
@@ -32,4 +32,5 @@ public class VehicleServiceMock {
         objectMapper.registerModule(new JavaTimeModule());
         return objectMapper;
     }
+   
 }
--- Managing_Vehicle_data/src/test/java/org/in/vehicle/service/impl/VehicleServiceImplTest.java
+++ Managing_Vehicle_data/src/test/java/org/in/vehicle/service/impl/VehicleServiceImplTest.java
@@ -2,6 +2,7 @@ package org.in.vehicle.service.impl;
 
 import org.in.vehicle.model.CarDetails;
 import org.in.vehicle.repository.VehicleRepository;
+import org.in.vehicle.service.exceptions.InvalidRequestException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,10 +12,12 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.in.vehicle.mock.VehicleServiceMock.getAddCarDetailsRequest;
 import static org.in.vehicle.mock.VehicleServiceMock.getCarDetails;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class VehicleServiceImplTest {
 
@@ -23,6 +26,9 @@ class VehicleServiceImplTest {
 
     @Mock
     VehicleRepository vehicleRepository;
+    
+    private final Integer vehicleId = 7;
+    
     @BeforeEach
     public void init(){
         MockitoAnnotations.openMocks(this);
@@ -36,5 +42,25 @@ class VehicleServiceImplTest {
         Mockito.when(vehicleRepository.save(request)).thenReturn(carDetails);
         CarDetails actualValue = vehicleService.saveCarDetails(request);
         assertEquals(carDetails, actualValue);
+    }
+    @Test
+    @DisplayName("Positive case: fetch car details by vehicle id")
+    void getCarDetailsById() throws IOException {
+        CarDetails expectedValue = getCarDetails();
+        Mockito.when(vehicleRepository.findById(vehicleId)).thenReturn(Optional.ofNullable(getCarDetails()));
+        CarDetails actualValue = vehicleService.getCarDetailsById(vehicleId);
+        assertEquals(expectedValue,actualValue);
+    }
+
+    @Test
+    @DisplayName("Negative case: fetch car details by vehicle id")
+    void getCarDetailsByIdNegativeCase(){
+        try {
+            Mockito.when(vehicleRepository.findById(vehicleId))
+                    .thenReturn(Optional.empty());
+            vehicleService.getCarDetailsById(vehicleId);
+        }catch (InvalidRequestException e){
+            assertTrue(true);
+        }
     }
 }
