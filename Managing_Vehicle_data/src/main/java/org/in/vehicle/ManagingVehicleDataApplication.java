cee2598a39bdbb536cbecb1a5b3a6c9b7f28cdb0
BhaktiKhichade <bhaktikhichade@gmail.com>
2023-04-03 15:41:21 +0530

    Created project repo

---
+++ Managing_Vehicle_data/src/main/java/org/in/vehicle/ManagingVehicleDataApplication.java
@@ -0,0 +1,13 @@
+package org.in.vehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingVehicleDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagingVehicleDataApplication.class, args);
+	}
+
+}
---
+++ Managing_Vehicle_data/src/test/java/org/in/vehicle/ManagingVehicleDataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.in.vehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagingVehicleDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
